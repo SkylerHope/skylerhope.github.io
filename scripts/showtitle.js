077c22b95a8a78a57786a37df881a05d9226772f
SkylerHope <elpidakouro13@gmail.com>
2025-07-28 13:42:00 +0300

    initial commit

---
+++ scripts/showtitle.js
@@ -0,0 +1,12 @@
+var titleName = document.getElementById('title_name');
+var titleGreet = document.getElementById('title_greet');
+
+if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
+    console.log('Page reloaded!');
+    titleName.style.display = "block";
+    if (titleName.style.display = "block") {
+        console.log('Name is now block');
+        titleGreet.style.display = "block";
+        console.log('Greet is now block too');
+    }
+}
